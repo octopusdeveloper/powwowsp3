db351b05d4fb223f1f8da8fb1d043191e944490d
Jorge Carlos Ceciliano Esquinca <jorge.cecilianoesquinca@flex.com>
2017-09-07 07:35:07 -0700

    [Studio] Modified screen "vlogin"

--- client/controllers/app/vlogin.js
+++ client/controllers/app/vlogin.js
@@ -1,5 +1,18 @@
 angular.module('app').controller('app_vlogin', app_vlogin);
-function app_vlogin($scope, app) {
+function app_vlogin($scope, app, $timeout) {
     'use strict';
     app.init($scope);
+    $scope.bLoading = false;
+    $scope.sLoginUser = '';
+    $scope.sLoginPass = '';
+    $scope.sTest="nd"
+    $scope.fLogin = function (sLoginUser, sLoginPass) {
+        console.log('sUser=' + sLoginUser);
+        console.log('sPass=' + sLoginPass);
+        $scope.bLoading = true;
+        var timer = $timeout(function () {
+            console.warn("move home page");
+            app.go('app.tasklist');
+        }, 2000);
+    };
 }
